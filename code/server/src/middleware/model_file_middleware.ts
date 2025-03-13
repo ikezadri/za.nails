ad2df35a42068d77e9c2e2c7bc00bebf5fe933d3
ikezadri <i.fatima.kezadri@gmail.com>
2025-03-13 12:43:17 +0100

    middleware encore

--- code/server/src/middleware/model_file_middleware.ts
+++ code/server/src/middleware/model_file_middleware.ts
@@ -1,12 +1,43 @@
 import type { NextFunction, Request, Response } from "express";
+import fs from "node:fs/promises";
+import ModelRepository from "../repository/model_repository.js";
+import type Model from "../model/model.js";class ModelFileMiddleware {
+	public process = async (req: Request, res: Response, next: NextFunction) => {
+		// récupérer le fichier transférer
+		const file = (req.files as Express.Multer.File[])[0];
 
-class ModelFileMiddleware {
-	public process = (req: Request, res: Response, next: NextFunction) => {
-        console.log("model file middleware");
-        
-        // passer le middleware suivant
-		next();
+		// récupérer l'enregistrement par son id
+		const model: Model | unknown = await new ModelRepository().selectOne(
+			req.body,
+		);
+
+		// si un fichier a été sélectionné
+		if (file) {
+			// ajouter un extension au nom du fichier
+			const filename = `${file.filename}.${file.mimetype.split("/")[1]}`;
+
+			// renommer le fichier transfére
+			await fs.rename(file.path, `${file.destination}/${filename}`);
+
+			// remplir la propriété de body en relation avec le fichier
+			req.body[file.fieldname] = filename;
+
+			// récupérer la méthode HTTP
+			// si une modification est effectuée, supprimer l'action fichier
+			if (req.method === "PUT") {
+				await fs.rm(`${file.destination}/${(model as Model).image}`);
+			}
+
+			// si un fichier n'a pas été sélectionné
+			else {
+				// PUT > récupérer le nom de l'ancienne image et l'affecter à la propriété gérant le fichier 
+
+				// DELETE > supprimer le fichier
+			}
+
+			// passer le middleware suivant
+			next();
+		}
 	};
 }
-
 export default ModelFileMiddleware;
