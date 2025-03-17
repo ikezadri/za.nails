4f9f161cebff7ca1c60909fb9c2d70c5ca58ff16
ikezadri <i.fatima.kezadri@gmail.com>
2025-03-17 19:07:54 +0100

    formulaire+probleme git regler

--- code/client/src/component/admin/model/AdminModelForm.tsx
+++ code/client/src/component/admin/model/AdminModelForm.tsx
@@ -1,78 +1,133 @@
 import { useForm } from "react-hook-form";
 import type Model from "../../../model/model";
+import ModelAPI from "../../../service/model_api";
+import { useEffect, useState } from "react";
+import type Types from "../../../model/types";
+// import TypeAPI from "../../../service/type_api";
+import TypesAPI from "../../../service/types_api";
 
 const AdminModelForm = () => {
-    // handSubmit = permet de gérer la soumission du formulaire
-    // register = permet de référencer les champs de formulaire
-    // errors = permet de gérer les messages d'erreur
-    const { handleSubmit, register, formState : {errors} } = useForm<Model>();
-    // soumission du formulaire
-    const onSubmitModel = (values: Model) => {
-        console.log(values);
-        
-    };
-    
-    return <form onSubmit={ handleSubmit( onSubmitModel )}>
-        <p>
-            <label htmlFor="name">Name:</label>
-            {/* reprendre STRICTEMENT le nom des colonnes SQL */}
-            <input type="text" {...register('name',{
-                required: "Nom requis",
-                minLength:{
-                    value: 2,
-                    message: "Nom trop court"
-                }
-            })} />
-            <small>{errors.name?.message}</small>
-        </p>
+	// handSubmit = permet de gérer la soumission du formulaire
+	// register = permet de référencer les champs de formulaire
+	// errors = permet de gérer les messages d'erreur
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm<Model>();
 
-        <p>
-            <label htmlFor="image">Image:</label>
-            {/* reprendre STRICTEMENT le nom des colonnes SQL */}
-            <input type="file" {...register('image',{
-                required: "Image requise",
-                minLength:{
-                    value:2,
-                    message: "Cette image est indisponible"
-                }
-            })} />
-            <small>{errors.image?.message}</small>
-        </p>
+	const [types, setTypes] = useState<Types[]>();
 
-        <p>
-            <label htmlFor="type">Type:</label>
-            {/* reprendre STRICTEMENT le nom des colonnes SQL */}
-            <input type="text" {...register('type',{
-                required: "Type requis",
-                minLength:{
-                    value:2,
-                    message:"Type requis"
-                }
-            })} />
-            <small>{errors.type?.message}</small>
-        </p>
+	useEffect(() => {
+		Promise.allSettled([new TypesAPI().selectAll()]).then(responses => {
+			if(responses[0].status === 'fulfilled'){
+				setTypes(responses[0].value.data);
+				console.log(responses[0].value.data);
+				
+			}
+		})
+	});
 
-        <p>
-            <label htmlFor="type_id">Type:</label>
-            {/* reprendre STRICTEMENT le nom des colonnes SQL */}
-            <input type="text" {...register('type_id',{
-                required: "Type requis",
-                minLength:{
-                    value:2,
-                    message:"Type requis"
-                }
-            })} />
-            <small>{errors.type_id?.message}</small>
-        </p>
+	// soumission du formulaire// values récupère la saisie du formulaire
 
+	// deux types de formulaire :
+	// - sans fichier la propriété body de la requête HTTP peut être en JSON : JSON.stringly / dans la requête HTTP, utiliser l'en tête : Content-Types : application json
 
-        
-        <p>
-            <button type="submit">Submit</button>
-        </p>
+	// - avec fichier
+	//  - la propriété body de la requête HTTP doit être en FormData
+	//  - la balise <form> doit posséder l'attribut enctype="multipart/form-data"
 
-    </form>;
+	const onSubmitModel = async (values: Model) => {
+		// créer un FormData en reprenant strictement le nom des champs
+		const formData = new FormData();
+		formData.append("name", values.name);
+		// un champ file renvoie une liste de fichiers (FileList)
+		formData.append("image", values.image);
+		formData.append("type", values.type);
+		// formData.append('type_id',values.type_id);
+		formData.append("type_ids", values.type_ids);
 
+		// console.log(formData);
+
+		// requête HTTP
+		const request = await new ModelAPI().insert(formData);
+        console.log(request);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmitModel)} encType="multipart/form-data">
+			<p>
+				<label htmlFor="name">Name:</label>
+				{/* reprendre STRICTEMENT le nom des colonnes SQL */}
+				<input
+					type="text"
+					{...register("name", {
+						required: "Nom requis",
+						minLength: {
+							value: 2,
+							message: "Nom trop court",
+						},
+					})}
+				/>
+				<small>{errors.name?.message}</small>
+			</p>
+
+			<p>
+				<label htmlFor="image">Image:</label>
+				{/* reprendre STRICTEMENT le nom des colonnes SQL */}
+				<input
+					type="file"
+					{...register("image", {
+						required: "Image requis",
+					})}
+				/>
+				<small>{errors.image?.message}</small>
+			</p>
+
+			<p>
+				<label htmlFor="type_ids">Type ids:</label>
+				{/* reprendre STRICTEMENT le nom des colonnes SQL */}
+				<input type="checkbox" id="type_ids" value="1" {...register("type_ids")} />
+
+				<input type="checkbox" id="type_ids" value="2" {...register("type_ids")} />
+
+				<small>{errors.type_ids?.message}</small>
+			</p>
+
+			<p>
+				<label htmlFor="type">Type:</label>
+				{/* reprendre STRICTEMENT le nom des colonnes SQL */}
+				<select 
+					id="type_id"
+						{...register("type_id",{
+							required:"Type requit"})}
+				>
+						<option value="">Sélectionne ton type</option>
+							{types?.map((type: Types) => {
+					return <option key={Math.random()} value={type.id}>
+						{type.name}
+					</option>;
+				})}
+
+				</select>
+				{/* <input
+					type="text"
+					{...register("type", {
+						required: "Type requis",
+						minLength: {
+							value: 2,
+							message: "Type requis",
+						},
+					})}
+				/> */}
+				<small>{errors.type?.message}</small>
+			</p>
+
+			<p>
+				<button type="submit">Submit</button>
+			</p>
+		</form>
+	);
 };
 
 export default AdminModelForm;
---
+++ code/client/src/service/types_api.ts
@@ -0,0 +1,18 @@
+class TypesAPI {
+    private route = 'types';
+    // récupération de tous les enregistrements
+    public selectAll = async () => {
+        
+        // configurer la requête HTTP
+        // import.meta.env permet d'accéder aux varibales d'environnement
+        const request = new Request(`${import.meta.env.VITE_API_URL}/${this.route}`);
+
+        // récuperer la réponse
+        const response = await fetch(request);
+
+        // renvoyer les résultats de la réponse 
+        return response.json();
+    };
+}
+
+export default TypesAPI;
